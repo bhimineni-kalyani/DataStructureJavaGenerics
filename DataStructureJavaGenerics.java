9a3f809fd90e475436081a862e40b78d785caf48
bhimineni-kalyani <bhiminenikalyani@gmail.com>
2021-03-11 21:05:32 +1400

    Refactor - program4 functionality added

--- DataStructureJavaGenerics.java
+++ DataStructureJavaGenerics.java
@@ -1,5 +1,50 @@
 public class DataStructureJavaGenerics {
-     public static void main(String[] args) {
-        System.out.println("Welcome to DataStructureJavaGenerics");
-     }
+
+    class Node{
+        int data;
+        Node Next;
+
+        public Node(int data){
+            this.data=data;
+            this.Next=null;
+        }
+    }
+
+    public Node head=null;
+    public Node tail=null;
+
+    public void addNode(int data) { 
+        Node newNode=new Node(data);
+        if (head==null) {
+            head=newNode;
+            tail=newNode;
+        }
+        else
+            tail.Next=newNode;
+            tail=newNode;
+    }
+
+    public void displayList() {  
+        Node current=head;
+        if (head==null) {
+            System.out.println("list is empty");
+        }
+        else {
+            System.out.println("single linked list: ");
+            while (current != null) {
+                System.out.println(current.data + "");
+                current = current.Next;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        DataStructureJavaGenerics list = new DataStructureJavaGenerics();
+
+        list.addNode(56);
+        list.addNode(30);
+        list.addNode(70);
+
+        list.displayList();
+    }
 }
